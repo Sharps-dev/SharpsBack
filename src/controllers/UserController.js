044df5da424d07ef24ae2e76d70bee8530817fd6
AmiraliPak <a4amirali@hotmail.com>
2021-03-15 01:31:10 +0330

    feat: add src files

---
+++ src/controllers/UserController.js
@@ -0,0 +1,15 @@
+const Controller =require('./Controller');
+const UserService = require("./../services/UserService");
+const User = require("./../models/User");
+
+const userService = new UserService(User);
+
+class UserController extends Controller {
+
+    constructor(service) {
+        super(service);
+    }
+
+}
+
+module.exports = new UserController(userService);
