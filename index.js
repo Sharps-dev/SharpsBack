393f9ca1d6b61ab000a56b7efaf8807e06fe2a25
Sajjad Ramezani <sajjadn47@gmail.com>
2021-03-14 19:27:47 +0330

    feat(index.js):add a simple api end point to test node ci workflow

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require('express')();
+const PORT = 8030;
+
+app.listen(
+    PORT,
+    () => console.log(`it's alive on http://localhost:${PORT}`)
+)
+
+app.get('/test' , (req , res) => {
+    res.status(200).send({
+        'message' : 'hi',
+        'user' : 'me from future'
+    })
+}); 
