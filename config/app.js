9767399ed9e60cc3cfd55e83ad092a60cf02e8e4
AmiraliPak <a4amirali@hotmail.com>
2021-03-15 21:56:12 +0330

    docs: setup swagger UI

--- config/app.js
+++ config/app.js
@@ -2,12 +2,17 @@ const express  = require('express');
 const bodyParser = require('body-parser');
 const setRoutes = require('./routes');
 const databaseConnect = require('./database');
+const swaggerUi = require("swagger-ui-express");
+const swaggerParser = require('swagger-parser');
+const path = require('path');
 
 const initiateApp = async () => {
 
     const app = express();
     setRoutes(app);
     databaseConnect();
+    const swagger_path = path.resolve(__dirname, './swagger.yaml');
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(await swaggerParser.bundle(swagger_path), { explorer: true }));
 
     app.use(bodyParser.json());
 
