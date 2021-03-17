26217f0651ae202286830c86852df0e2312b7d34
Reza Mousavian <rezamosavian1010@gmail.com>
2021-03-17 23:25:20 +0330

    fix(login) : fix function property

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -8,41 +8,40 @@ class UserService extends Service {
     super(model);
     this.login = this.login.bind(this);
   }
-}
-
-/**
- * login function user
- * @param {String} data
- */
-async function login(data) {
-  try {
-    const { password, email } = data;
-    const user = await this.model.findOne({ email });
-    if (!user)
+  /**
+   * login function user
+   * @param {String} data
+   */
+  async login(data) {
+    try {
+      const { password, email } = data;
+      const user = await this.model.findOne({ email });
+      if (!user)
+        return {
+          error: true,
+          status: 400,
+          message: "check your email",
+        };
+      const checkUserPassword = await user.checkPassword(password);
+      if (!checkUserPassword)
+        return {
+          error: true,
+          status: 400,
+          message: "check your password",
+        };
+      return {
+        error: false,
+        statusCode: 200,
+        data: user,
+      };
+    } catch (errors) {
+      console.error(errors);
       return {
         error: true,
         status: 400,
-        message: "check your email",
+        message: "check input valid thing",
       };
-    const checkUserPassword = await user.checkPassword(password);
-    if (!checkUserPassword)
-      return {
-        error: true,
-        status: 400,
-        message: "check your password",
-      };
-    return {
-      error: false,
-      statusCode: 200,
-      data: user,
-    };
-  } catch (errors) {
-    console.error(errors);
-    return {
-      error: true,
-      status: 400,
-      message: "check input valid thing",
-    };
+    }
   }
 }
 
