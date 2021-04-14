af387222cd4afbb580706b7e140469fce62be82a
Reza Mousavian <rezamosavian1010@gmail.com>
2021-04-14 10:38:24 +0430

    feat : add controller and service

---
+++ src/controllers/ContentController.js
@@ -0,0 +1,14 @@
+﻿const Controller = require("./Controller");
+const ContentService = require("../services/ContentService");
+const Content = require("../models/Content");
+const { AppError } = require("../helpers/AppError");
+
+const contentService = new ContentService(Content);
+
+class ContentController extends Controller {
+  constructor(service) {
+    super(service);
+  }
+}
+
+module.exports = new ContentController(contentService);
