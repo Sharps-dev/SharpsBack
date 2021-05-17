556f6b1add913b1689a05795c5e5693e98e3cd0b
AmiraliPak <a4amirali@hotmail.com>
2021-05-17 21:20:55 +0430

    feat(unlike): add unlike userHistory event

--- src/services/UserHistoryService.js
+++ src/services/UserHistoryService.js
@@ -1,7 +1,22 @@
+const { MissingBodyFieldError, AppError } = require("../helpers/AppError");
 const Service = require("./Service");
 class UserHistoryService extends Service {
     constructor(model) {
         super(model);
+        this.insert = this.insert.bind(this);
+    }
+
+    async insert(data) {
+        const eventFunc = {
+            UNLIKE: async (data) => {
+                if (!data.content) throw new MissingBodyFieldError();
+                if (!data.user) throw new AppError('', 500);
+                data.eventType = 'LIKE';
+                await this.model.deleteOne(data);
+            }
+        }
+        if (eventFunc[data.eventType]) await eventFunc[data.eventType](data);
+        else return await super.insert(data);
     }
 }
 
