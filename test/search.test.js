85c3ed086aac2a2f5197e28e6eaa5604bd19a6fd
AmiraliPak <a4amirali@hotmail.com>
2021-07-11 01:14:13 +0430

    test(tags): add tests for tag filter and search

--- test/search.test.js
+++ test/search.test.js
@@ -7,10 +7,10 @@ const contentModel = require('../src/models/Content');
 const newToken = require("../src/utils/jwt/newToken");
 
 const contents = [
-    { image: "a", domain: "https://a.com", path: "/a", des: "a", title: "a" },
-    { image: "b", domain: "https://b.com", path: "/b", des: "b", title: "b" },
-    { image: "c", domain: "https://c.com", path: "/c", des: "c", title: "c" },
-    { image: "d", domain: "https://d.com", path: "/d", des: "d", title: "d" }
+    { image: "a", domain: "https://a.com", path: "/a", des: "a", title: "a", tags:["holu"] },
+    { image: "b", domain: "https://b.com", path: "/b", des: "b", title: "b", tags:["moz", "goje"] },
+    { image: "c", domain: "https://c.com", path: "/c", des: "c", title: "c", tags:["hendoone", "moz"] },
+    { image: "d", domain: "https://d.com", path: "/d", des: "d", title: "d", tags:["goje"] }
 ];
 const userInfo = { firstname: "John2", lastname: "Doe", email: "john2@doe.com", username: "john2d", password: "123456" };
 
@@ -36,7 +36,7 @@ describe('search tests 🔍', () => {
     it('searches on contents no filters', async function () {
         let res = await request.get('/content/search').set({ Authorization: 'Bearer ' + token }).query({ s: 'b' });
         expect(res.status).to.equal(200);
-        expect(res.body.items[0]).to.include(contents[1]);
+        expect(res.body.items[0]).to.deep.include(contents[1]);
         res = await request.get('/content/search').set({ Authorization: 'Bearer ' + token }).query({ s: '.com' });
         expect(res.status).to.equal(200);
         expect(res.body.items).to.have.lengthOf(4);
@@ -45,18 +45,31 @@ describe('search tests 🔍', () => {
     it('searches on content urls', async function () {
         let res = await request.get('/content/search').set({ Authorization: 'Bearer ' + token }).query({ s: 'b.c', url: 'true' });
         expect(res.status).to.equal(200);
-        expect(res.body.items[0]).to.include(contents[1]);
+        expect(res.body.items[0]).to.deep.include(contents[1]);
         res = await request.get('/content/search').set({ Authorization: 'Bearer ' + token }).query({ s: 'https://a.com', url: 'true' });
         expect(res.status).to.equal(200);
-        expect(res.body.items[0]).to.include(contents[0]);
+        expect(res.body.items[0]).to.deep.include(contents[0]);
     });
 
     it('searches on content titles', async function () {
         let res = await request.get('/content/search').set({ Authorization: 'Bearer ' + token }).query({ s: 'd', title: 'true' });
         expect(res.status).to.equal(200);
-        expect(res.body.items[0]).to.include(contents[3]);
+        expect(res.body.items[0]).to.deep.include(contents[3]);
         res = await request.get('/content/search').set({ Authorization: 'Bearer ' + token }).query({ s: 'abcd', title: 'true' });
         expect(res.status).to.equal(200);
         expect(res.body.items).to.be.empty;
+    });
+
+    it('searches on content tags', async function () {
+        let res = await request.get('/content/search').set({ Authorization: 'Bearer ' + token }).query({ s: 'moz', tags: 'true' });
+        expect(res.status).to.equal(200);
+        expect(res.body.items.length).to.equal(2);
+        res = await request.get('/content/search').set({ Authorization: 'Bearer ' + token }).query({ s: 'khiar', tags: 'true' });
+        expect(res.status).to.equal(200);
+        expect(res.body.items.length).to.equal(0);
+        res = await request.get('/content/search').set({ Authorization: 'Bearer ' + token }).query({ s: 'holu', tags: 'true' });
+        expect(res.status).to.equal(200);
+        expect(res.body.items.length).to.equal(1);
+        expect(res.body.items[0]).to.deep.include(contents[0]);
     });
 });
--- test/suggestion.test.js
+++ test/suggestion.test.js
@@ -7,10 +7,10 @@ const contentModel = require('../src/models/Content');
 const newToken = require("../src/utils/jwt/newToken");
 
 const contents = [
-    { image: "a", domain: "https://a.com", path: "/a", des: "a", title: "a" },
-    { image: "b", domain: "https://b.com", path: "/b", des: "b", title: "b" },
-    { image: "c", domain: "https://c.com", path: "/c", des: "c", title: "c" },
-    { image: "d", domain: "https://d.com", path: "/d", des: "d", title: "d" }
+    { image: "a", domain: "https://a.com", path: "/a", des: "a", title: "a", tags:["holu"] },
+    { image: "b", domain: "https://b.com", path: "/b", des: "b", title: "b", tags:["moz", "goje"] },
+    { image: "c", domain: "https://c.com", path: "/c", des: "c", title: "c", tags:["hendoone", "moz"] },
+    { image: "d", domain: "https://d.com", path: "/d", des: "d", title: "d", tags: ["goje"] }
 ];
 const userInfo = { firstname: "John2", lastname: "Doe", email: "john2@doe.com", username: "john2d", password: "123456" };
 
@@ -19,6 +19,7 @@ describe('suggestion tests', () => {
     let suggesters;
     let suggestions;
     let user;
+    let token;
 
     before(async () => {
         await contentModel.deleteMany();
@@ -37,11 +38,17 @@ describe('suggestion tests', () => {
     });
 
     it("gets a user's suggestions", async () => {
-        const token = newToken(user);
+        token = newToken(user);
         user.tokens.push(token);
         await user.save();
         const res = await request.get('/user/suggestions').set({ Authorization: 'Bearer ' + token }).query({ showAds: false });
         expect(res.status).to.equal(200);
-        expect(res.body.total).to.equal(4);
-    })
+        expect(res.body.items).to.have.lengthOf(4);
+    });
+
+    it("filter tags in explore", async () => {
+        const res = await request.get('/user/suggestions?showAds=false&tags[]=hendoone').set({ Authorization: 'Bearer ' + token });
+        expect(res.status).to.equal(200);
+        expect(res.body.items).to.have.lengthOf(1);
+    });
 });
